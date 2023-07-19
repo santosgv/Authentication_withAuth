58012e0d3d467cc9e99afb8549d824217939ffe8
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-07-19 20:30:20 -0300

    front em vue

---
+++ front/src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>LOGIN</h1>
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="username" />
+      <br />
+      <br />
+      <input v-model="password" placeholder="password" type="password" />
+      <br />
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+async login(e) {
+      e.preventDefault();
+      const response = await fetch("http://127.0.0.1:8000/api/auth/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password,
+        }),
+      });
+    },
+  },
+};
+</script>
