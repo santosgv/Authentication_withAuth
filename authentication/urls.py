b053f854c7c3ce5fa57b55c42c5e33d0dba1b876
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-07-19 16:00:30 -0300

    primeito commit

---
+++ authentication/urls.py
@@ -0,0 +1,17 @@
+from dj_rest_auth.jwt_auth import get_refresh_view
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
+from django.urls import path
+from rest_framework_simplejwt.views import TokenVerifyView
+from allauth.socialaccount.views import signup
+from authentication.views import GoogleLogin
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="rest_register"),
+    path("login/", LoginView.as_view(), name="rest_login"),
+    path("logout/", LogoutView.as_view(), name="rest_logout"),
+    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
+    path("google/", GoogleLogin.as_view(), name="google_login"),
+]
